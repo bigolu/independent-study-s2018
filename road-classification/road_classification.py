5ee5fad740b4512ab82e1cc87ef9d4fee6dbf682
Biggie Emmanuel <bigolu96@gmail.com>
2018-03-05 23:44:36 -0500

    move some stuff around

--- road-classification/road_classification.py
+++ road-classification/road_classification.py
@@ -1,12 +1,20 @@
+"""
+An SVC for road types.
+"""
+from typing import List, Tuple
+import sys
+
 from sklearn import svm
 import numpy as np
 
+
 CACHE_SIZE = 1000
 
-road_classifier = None
 
-
-def debug():
+def debug() -> None:
+    """
+    for testing
+    """
     training_features = np.ascontiguousarray([[0, 0], [1, 1]],
                                              dtype=np.float32)
     training_labels = np.ascontiguousarray([0, 1], dtype=np.float32)
@@ -22,27 +30,43 @@ def debug():
 
     print(road_classifier.predict([[2., 2.]]))
 
+def load_data(filenames: List[str]) -> List[Tuple[str]]:
+    """
+    Load data from csv file(s). Returns a list of tuples where each tuple
+    represents a record. The structure of tuple is as follows:
+    0:
+    1:
+    2:
+    3:
 
-def train():
+    Args:
+    files - a list of filenames for CSVs containing data
+    """
+    records = []
+    return records
+
+def make_feature_vectors() -> List[List[float]]:
     pass
 
+def train():
+    feature_vectors = []
+    labels = []
 
 def test():
     pass
 
-
-def predict():
-    pass
-
-
-def classifier_init():
-    global road_classifier
+def main():
+    """
+    Classify roads.
+    """
+    raw_data = load_data(sys.argv[1:])
+    feature_vectors  = make_feature_vectors(raw_data)
+    labels = make_labels(raw_data)
     road_classifier = svm.SVC(CACHE_SIZE)
-
+    train()
+    # test()
 
 if __name__ == '__main__':
-    classifier_init()
-    debug()
-    # train()
-    # test()
-    # predict()
+    main()
+    # debug()
+
